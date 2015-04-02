9ed2e7bc1f62274eff5d1e7deb395742aa1941ac
MoinePygmee <crosbastien@hotmail.fr>
2015-04-02 11:09:05 +0200

    Ajout du fichier matrice.c : fichier main

---
+++ matrice.c
@@ -0,0 +1,17 @@
+/*
+*			main.c
+*	 un petit .c qui va générer un matrice où des millions de gens 
+*	pourrons vivre heureux ;-)
+*/
+
+#include <lib.h>
+
+#define world_pop 	100000
+#define world_size 	20
+
+int main()
+{
+
+	return 0;
+
+}
